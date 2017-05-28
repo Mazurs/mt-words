aab2b3e53a10bf63a806a41dbdfd5898cebcd7da
Rūdolfs Mazurs <rudolfsm@src.gnome.org>
2017-05-28 21:31:00 +0300

    Moved some routines to functions, added unittests

---
+++ po_dictum.py
@@ -0,0 +1,217 @@
+import re
+import string
+
+#from libs.dictionary import dictionary
+from libs import dictionary
+
+# List of elements, that contains untranslatable strings
+escapeables = [
+    '<.*?>', # Tags
+    '%[diuoxXfFeEgGaAcspn]', # C style variables, simple
+    '%\([word]*\)[diuoxXfFeEgGaAcspn]', # C style variables, named
+    '%[0-9]\$[diuoxXfFeEgGaAcspn]', # C style variables, positions indicated
+    '&[\w|\.]*;', # Mozilla style variables
+    'http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+', # URLs
+    '\s&\s', # Mozilla style "and" replacement. FIXME Dubious.
+]
+# FIXME what is dis?"<*>"
+class fragment:
+    """Container for string fragments.
+    Attributes:
+        text: fragment of translation text
+        flag:   status of the text, possible values:
+            word - translateable word
+            pending - potentially translateable word
+            tag - untranslatable tag name or attribute
+            var - untranslatable variable
+            exist - untranslatable, because exists
+            scrap - untranslatable non-letter characters
+            other
+    """
+    def __init__(self,text,flag="pending"):
+
+        assert flag in ["word", "pending", "tag", "var", "exist", "scrap", "other"]
+
+        self.text = text
+        self.flag = flag
+        self.new_text = "" # FIXME is this real?
+        self.found_accelerator = False
+
+class word_substitute:
+    """Worker that does dictionary replacement"""
+
+    def __init__ (self, dictionary_file, project = "GNOME", missing=None):
+        self.table = dictionary (dictionary_file)
+        # TODO implement missing word list
+        self.project = project # TODO later extract behaviour
+
+    def substitute (self, unit):
+        source = unit.getsource()
+        target = unit.gettarget()
+
+        # Cut out undesirables FIXME take project into consideration
+
+        source_fragments = exclude (source, escapeables, "other")
+        target_fragments = exclude (target, escapeables, "other")
+
+        # Remove accelerator
+
+        source_fragments, source_accl = remove_accelerator(source_fragments, "_") #FIXME hardcode
+        target_fragments, target_accl = remove_accelerator(target_fragments, "_")
+
+        # Strip out words
+
+        source_fragments = exclude (source_fragments, '[\W]', "word")
+        target_fragments = exclude (target_fragments, '[\W]', "word")
+
+        # Mark duplicates
+
+        for t in target_fragments:
+            if t.flag == "word":
+                for s in source_fragments:
+                    if s.flag == "word" and s.text == t.text:
+                        t.flag = "exist"
+
+        # Replace words
+
+        for t in target_fragments:
+            if t.flag == "word":
+                word_type = identify_case(t.text)
+                translation = self.table.find(t.text.lower())
+                if translation:
+                    t.text = translation
+                else:
+                    unit.markfuzzy()
+
+        # Put back accelerator FIXME is this really an improvement?
+
+        found = False
+        for t in target_fragments:
+            if t.flag in ["word", "pending", "exist"]:
+                replacement = place_accelerator(t.text, target_char, "_")
+                if replacement:
+                    t.text = replacement
+                    found = True
+                    break
+        if not found:
+            for t in target_fragments:
+                if t.flag in ["word", "pending", "exist"]:
+                    replacement = place_accelerator(t.text, source_char, "_")
+                    if replacement:
+                        t.text = replacement
+                        found = True
+                        break
+        if not found:
+            for t in target_fragments:
+                if t.flag in ["word", "pending", "exist"]:
+                    t.text = "_" + t.text
+                    break
+
+        # Collapse
+
+        output = ""
+        for t in target_fragments:
+            output += t.text
+        unit.settarget(output)
+
+def exclude (original, exclude, flag):
+    if type(original) is str:
+        given = [fragment(original)]
+    elif type (original) is list:
+        given = original
+    else:
+        given = [original]
+
+    if type(exclude) is not list:
+        exclude = [exclude]
+    #print(source_fragment.text)
+
+    # New compile
+    for reg in escapeables:
+        compiled = re.compile(reg)
+        derivative = []
+
+        for i in given: print (i.text)
+
+        for index, fragment in enumerate(given):
+            if fragment.flag != "pending":
+                derivative.append(fragment)
+                continue
+            subfragments = []
+            start = 0
+            for pattern in compiled.finditer(fragment.text):
+                pattern_end = pattern.start() + len(pattern.group())
+                subfragments.append( fragment( fragment.text[start:pattern.start()], "pending" ) )
+                subfragments.append( fragment( fragment.text[pattern.start():pattern_end], flag) )
+                start = pattern_end
+            subfragments.append(fragment( fragment.text[start:len(fragment.text)]))
+            derivative += subfragments
+        given = derivative
+    return derivative
+
+def remove_accelerator(source_fragments, accelerator):
+    source_char = None
+    for f in source_fragments:
+        if f.flag == "pending":
+            response = remove_accel (f.text, accelerator)
+            if (response == None): continue
+            source_char, text = response
+            f.text = text
+            f.found_accelerator = True
+            break
+    return source_fragments, source_char
+
+def remove_accel(text, accelerator):
+    pos = text.find(accelerator)
+    if pos != -1:
+        accel_char = text[pos+1] #storing accelerator char
+        return accel_char, text[:pos] + text[pos+1:] #FIXME might fail if accel is last
+
+def place_accel(text, accelerator, symbol):
+    acc_pos = text.lower().find(accelerator.lower())
+    if acc_pos != -1:
+        return text[:acc_pos] + symbol + text[acc_pos:]
+    else:
+        return None
+
+def identify_case(word):
+    if word.islower():
+        return 'lower'
+    if word.isupper():
+        return 'upper'
+    if word.istitle():
+        return 'sentence'
+    return 'weird'
+
+def restore_case(word, s_type):
+    # string should be in lowercase
+    if s_type == 'lower':
+        return word
+    if s_type == 'upper':
+        return word.upper()
+    if s_type == 'sentence':
+        return word[0:1].upper() + word[1:]
+    # Case with 'weird' capitalization is omitted
+    return word
+
+def mtfile(inputfile, outputfile, templatefile, dictionary):
+    inputstore = factory.getobject(inputfile)
+    if inputstore.isempty():
+        return 0
+    convertor = dict_translate(dictionary)
+    outputstore = convertor.convertstore(inputstore)
+    outputfile.write(str(outputstore))
+    return 1
+
+
+def main():
+    from translate.convert import convert
+    formats = {"po": ("po", mtfile), "xlf": ("xlf", mtfile), "tmx": ("tmx", mtfile)}
+    parser = convert.ConvertOptionParser(formats, usepots=True, description=__doc__)
+    parser.add_option("-d", "--dictionary", dest="dictionary",
+                      help="Dictionary file. Record format: key<tab>translation<newline>")
+    parser.passthrough.append("dictionary")
+    parser.run()
+
+if __name__ == '__main__':
+    main()
---
+++ test/test_utils.py
@@ -0,0 +1,77 @@
+import po_dictum as p
+import unittest
+
+class TestIdentifyType(unittest.TestCase):
+
+    def test_identify_case(self):
+        self.assertEqual(p.identify_case('foo'), 'lower')
+        self.assertEqual(p.identify_case('FOO'), 'upper')
+        self.assertEqual(p.identify_case('Foo'), 'sentence')
+        self.assertEqual(p.identify_case('FoO'), 'weird')
+        self.assertEqual(p.identify_case(''), 'weird')
+        self.assertEqual(p.identify_case('123'), 'weird')
+        with self.assertRaises(AttributeError):
+            p.identify_case(2)
+
+class TestRestoreCase(unittest.TestCase):
+
+    def test_restore_case(self):
+        self.assertEqual(p.restore_case('foo','lower'), 'foo')
+        self.assertEqual(p.restore_case('foo','upper'), 'FOO')
+        self.assertEqual(p.restore_case('foo','sentence'), 'Foo')
+        self.assertEqual(p.restore_case('foo','weird'), 'foo')
+        self.assertEqual(p.restore_case('','weird'), '')
+        self.assertEqual(p.restore_case('','upper'), '')
+        self.assertEqual(p.restore_case('123','upper'), '123')
+
+
+class TestRemoveAcceleratorFromFragment(unittest.TestCase):
+
+    def test_remove(self):
+        self.assertEqual(p.remove_accel('_Sveiks', '_'),('S','Sveiks'))
+        self.assertEqual(p.remove_accel('Sveiks', '_'),None)
+        self.assertEqual(p.remove_accel('&Sveiks', '&'),('S','Sveiks'))
+
+class TestPlaseAcceleratorIntoFragment(unittest.TestCase):
+
+    def test_place_at_beginning(self):
+        self.assertEqual(p.place_accel('Sveiks', 'S', '_'),('_Sveiks'))
+        self.assertEqual(p.place_accel('Sveiks', 's', '_'),('_Sveiks'))
+
+    def test_place2(self):
+        self.assertEqual(p.place_accel('Sveiks', 'v', '_'),('S_veiks'))
+
+class TestRemoveAcceleratorFromListOfFragments(unittest.TestCase):
+
+    def test_remove_from_a_single_fragment(self):
+        l = [p.fragment("_Sveiks")]
+        frag_list, acc_char = p.remove_accelerator(l, "_")
+        self.assertEqual(frag_list[0].text,"Sveiks")
+        self.assertEqual(frag_list[0].found_accelerator,True)
+        self.assertEqual(acc_char,"S")
+
+    def test_fail_to_remove_from_a_single_fragment(self):
+        l = [p.fragment("Sveiks")]
+        frag_list, acc_char = p.remove_accelerator(l, "_")
+        self.assertEqual(frag_list[0].text,"Sveiks")
+        self.assertEqual(frag_list[0].found_accelerator,False)
+        self.assertEqual(acc_char,None)
+
+    def test_remove_from_several_fragments(self):
+        l = [p.fragment("Sveiks"),p.fragment("d_raugs")]
+        frag_list, acc_char = p.remove_accelerator(l, "_")
+        self.assertEqual(frag_list[0].text,"Sveiks")
+        self.assertEqual(frag_list[0].found_accelerator,False)
+        self.assertEqual(frag_list[1].text,"draugs")
+        self.assertEqual(frag_list[1].found_accelerator,True)
+        self.assertEqual(acc_char,"r")
+
+    def test_fail_to_remove_from_a_specal_fragment(self):
+        l = [p.fragment("_neaiztiec","var")]
+        frag_list, acc_char = p.remove_accelerator(l, "_")
+        self.assertEqual(frag_list[0].text,"_neaiztiec")
+        self.assertEqual(frag_list[0].found_accelerator,False)
+        self.assertEqual(acc_char,None)
+
+if __name__ == '__main__':
+    unittest.main()
